d035cb25a9deb97fb546d7ba45e71dc74b5af2a1
Mohammad Khalil <mohhammad.samer21@outlook.com>
2026-05-30 05:08:33 +0300

    mobile ui improvements

--- client/user/src/components/common/MobileBottomNav.jsx
+++ client/user/src/components/common/MobileBottomNav.jsx
@@ -26,7 +26,11 @@ const MobileBottomNav = () => {
     <div className="fixed bottom-0 left-0 right-0 z-[999] md:hidden bg-black/95 border-t border-[#9DFF00] pb-2">
       <div className="grid grid-cols-4 text-center drop-shadow-[0_0_8px_#9DFF00]">
 
-        <NavLink to="/auth/profile" className={itemClass}>
+        <NavLink
+  to="/auth/profile"
+  onClick={() => window.scrollTo(0, 0)}
+  className={itemClass}
+>
           <User size={22} className={iconClass} />
           <span>الصفحة الشخصية</span>
         </NavLink>
@@ -39,12 +43,20 @@ const MobileBottomNav = () => {
           <span>الحجوزات</span>
         </button>
 
-        <NavLink to="/auth/leaderboard" className={itemClass}>
+        <NavLink
+  to="/auth/leaderboard"
+  onClick={() => window.scrollTo(0, 0)}
+  className={itemClass}
+>
           <Trophy size={22} className={iconClass} />
           <span>المتصدرون</span>
         </NavLink>
 
-        <NavLink to="/" className={itemClass}>
+        <NavLink
+  to="/"
+  onClick={() => window.scrollTo(0, 0)}
+  className={itemClass}
+>
           <Home size={22} className={iconClass} />
           <span>الرئيسية</span>
         </NavLink>
